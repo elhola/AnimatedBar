98f1bb24dac64ca37832a7e50ef8e6f870127cd4
Yaroslav G <yarmcfly@gmail.com>
2023-06-14 10:45:08 +0300

    Initial Commit

---
+++ AnimatedBar/AnimatedBarApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimatedBarApp.swift
+//  AnimatedBar
+//
+//  Created by Yaroslav G on 14.06.2023.
+//
+
+import SwiftUI
+
+@main
+struct AnimatedBarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AnimatedBar/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  AnimatedBar
+//
+//  Created by Yaroslav G on 14.06.2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
