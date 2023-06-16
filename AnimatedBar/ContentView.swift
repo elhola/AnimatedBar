91544d6b2d9bf51dc3990e014884a0efd4360183
Yaroslav G <yarmcfly@gmail.com>
2023-06-16 10:11:32 +0300

    update

--- AnimatedBar/ContentView.swift
+++ AnimatedBar/ContentView.swift
@@ -8,19 +8,40 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var tabSelected: Tab = .house
+    
+    init() {
+        UITabBar.appearance().isHidden = true
+    }
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        ZStack {
+            VStack {
+                TabView(selection: $tabSelected) {
+                    ForEach(Tab.allCases, id: \.rawValue) { tab in
+                        HStack {
+                            Image(systemName: tab.rawValue)
+                            Text("\(tab.rawValue.capitalized)")
+                                .bold()
+                                
+                                .animation(nil, value: tabSelected)
+                        }
+                        .tag(tab)
+                        
+                    }
+                }
+            }
+            VStack {
+                Spacer()
+                CustomTabBar(selectedTab: $tabSelected)
+            }
         }
-        .padding()
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+            .previewInterfaceOrientation(.portrait)
     }
 }
---
+++ AnimatedBar/CustomTabBar.swift
@@ -0,0 +1,68 @@
+//
+//  CustomTabBar.swift
+//  AnimatedBar
+//
+//  Created by Yaroslav G on 14.06.2023.
+//
+
+import SwiftUI
+
+enum Tab: String, CaseIterable {
+    case house
+    case message
+    case person
+    case moon
+    case gearshape
+}
+
+struct CustomTabBar: View {
+    @Binding var selectedTab: Tab
+    private var fillImage: String {
+        selectedTab.rawValue + ".fill"
+    }
+    private var tabColor: Color {
+        switch selectedTab {
+        case .house:
+            return .green
+        case .message:
+            return .blue
+        case .person:
+            return .yellow
+        case .moon:
+            return .mint
+        case .gearshape:
+            return .indigo
+        }
+    }
+    
+    
+    var body: some View {
+        VStack {
+            HStack {
+                ForEach(Tab.allCases, id: \.rawValue) { tab in
+                    Spacer()
+                    Image(systemName: selectedTab == tab ? fillImage : tab.rawValue)
+                        .scaleEffect(tab == selectedTab ? 1.25 : 1.0)
+                        .foregroundColor(tab == selectedTab ? tabColor : .gray)
+                        .font(.system(size: 20))
+                        .onTapGesture {
+                            withAnimation(.easeInOut(duration: 0.1)) {
+                                selectedTab = tab
+                            }
+                        }
+                    Spacer()
+                }
+            }
+            .frame(width: nil, height: 60)
+            .background(.thinMaterial)
+            .cornerRadius(20)
+            .padding()
+        }
+    }
+}
+
+struct CustomTabBar_Previews: PreviewProvider {
+    static var previews: some View {
+        CustomTabBar(selectedTab: .constant(.house))
+    }
+}
